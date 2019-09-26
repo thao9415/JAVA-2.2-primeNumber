4e3a0f37a27226f1ee30c13beb251ff2d8eaaa89
thao9415 <thao.9415@gmail.com>
2019-09-26 09:20:51 +0700

    check prime number

---
+++ src/PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if (num < 2) {
+            System.out.println(num + " is not a prime number");
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(num)) {
+                if (num % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check) {
+                System.out.println(num + " is a prime number");
+
+            } else {
+                System.out.println(num + " is not a prime number");
+            }
+
+        }
+
+    }
+}
